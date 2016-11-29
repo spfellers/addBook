5dd98ddb193639a56cc485cf92a05958cc3e0644
Samuel P. Fellers <sfellers@moore04.cs.purdue.edu>
2016-11-28 21:42:55 -0500

    add js function to scroll instead of jump to address div

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -46,4 +46,12 @@ $(function() {
 
 		});
 
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
+
 });
