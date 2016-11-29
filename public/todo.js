a8647929d1db3835ef52603ccd9d53fd3fbed4bd
Samuel P. Fellers <sfellers@moore04.cs.purdue.edu>
2016-11-28 21:28:58 -0500

    angular commit

--- public/todo.js
+++ public/todo.js
@@ -1,30 +1,39 @@
     angular.module('todoApp', [])
       .controller('TodoListController', function() {
+
         var todoList = this;
         todoList.todos = [
-          {text:'learn angular', done:true},
-          {text:'build an angular app', done:false}];
-     
-        todoList.addTodo = function() {
-          todoList.todos.push({text:todoList.todoText, done:false});
-          todoList.todoText = '';
-        };
-     
-        todoList.remaining = function() {
-          var count = 0;
-          angular.forEach(todoList.todos, function(todo) {
-            count += todo.done ? 0 : 1;
-          });
-          return count;
-        };
-     
-        todoList.archive = function() {
-          var oldTodos = todoList.todos;
-          todoList.todos = [];
-          angular.forEach(oldTodos, function(todo) {
-            if (!todo.done) todoList.todos.push(todo);
-          });
-        };
-      });
+          {names:'Sally 	1433 Elm Street 555-534-2345', done:false},
+          {names:'Bob 	1240 Pennsylvania Street 555-435-4547', done:false}
+		];
 
+		todoList.names = [
+          {names:'Sally', address:'438 djfhui street', phone:'555-555-5555', done:false},
+          {names:'Bob', address:'932 werewr avenue', phone:'473-234-2398', done:false}
+		];
+     
+		todoList.addTodo = function() {
+		  todoList.names.push({names:todoList.todoText, address:todoList.add, phone:todoList.phone, done:false});
+		  todoList.todoText = '';
+		  todoList.add = '';
+		  todoList.phone = '';
+		};
+	 
+		todoList.remaining = function() {
+		  var count = 0;
+		  angular.forEach(todoList.todos, function(todo) {
+		    count += todo.done ? 0 : 1;
+		  });
+		  return count;
+		};
+	 
+		todoList.archive = function() {
+		  var oldTodos = todoList.todos;
+		  todoList.todos = [];
+		  todoList.names = [];
+		  angular.forEach(oldTodos, function(todo) {
+		    if (!todo.done) todoList.todos.push(todo);
+		  });
+		};
+	  });
 
