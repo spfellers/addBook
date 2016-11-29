d26c6e930015cd47246376b8fa1f35a2601b7ff6
Samuel P. Fellers <sfellers@moore04.cs.purdue.edu>
2016-11-28 20:04:55 -0500

    commit

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-/*
+
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
-*/
+
