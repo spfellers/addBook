dbadec3d72a748539bef9412a89db85f38d05f78
Sam Fellers <sfellers@cs.purdue.edu>
2016-12-04 22:16:25 +0000

    tt

--- public/test.js
+++ public/test.js
@@ -1,3 +1,5 @@
+/*globals angular */
+/*eslint-env browser */
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
