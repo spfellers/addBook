62211f822f442cedc2a050cee19ebb9c82ef60e4
Samuel P. Fellers <sfellers@moore03.cs.purdue.edu>
2016-12-04 14:10:03 -0500

    test connections with node

--- main.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-
-fs.readFile('./public/index.html', function (err, data) {
-   if (err){
-      console.log(err.stack);
-      return;
-   }
-   console.log(data.toString());
-});
