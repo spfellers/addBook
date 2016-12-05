1cf2828f4ccdda6582a9697b489ff2eda99d97a7
Samuel P. Fellers <sfellers@moore12.cs.purdue.edu>
2016-12-04 22:37:40 -0500

    Commit addBook

--- server.js
+++ server.js
@@ -20,10 +20,7 @@ var url = 'mongodb://admin:RADYQZUDBEYHLWVT@sl-us-dal-9-portal.3.dblayer.com:169
 
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
-    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-    app.use(bodyParser.json());                                     // parse application/json
-    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-    app.use(methodOverride());
+	app.use(bodyParser.json());
 	var appEnv = cfenv.getAppEnv();
 
 
@@ -32,8 +29,6 @@ var url = 'mongodb://admin:RADYQZUDBEYHLWVT@sl-us-dal-9-portal.3.dblayer.com:169
         res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
     // listen (start app with node server.js) ======================================
-    app.listen(appEnv.port);
-    console.log("App listening on port ",appEnv.port);
 
 /*
 
@@ -171,3 +166,6 @@ app.put('/contactlist/:id', function (req, res) {
 });
 
 */
+
+    app.listen(appEnv.port);
+    console.log("App listening on port ",appEnv.port);
